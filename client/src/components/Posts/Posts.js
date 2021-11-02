1a928269fcc33e3afaabf6038eea96cf866ddf91
yazgeldigithub <devster2016@gmail.com>
2021-11-02 13:04:58 -0500

    components are finished

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,16 +1,31 @@
 import React from "react";
-import Post from "./Post/Post";
+import { Grid, CircularProgress } from "@material-ui/core";
+import { useSelector } from "react-redux";
 
+import Post from "./Post/Post";
 import useStyles from "./styles";
 
-const Posts = () => {
+const Posts = ({ setCurrentId }) => {
+  const { posts, isLoading } = useSelector((state) => state.posts);
   const classes = useStyles();
-  return (
-    <>
-      <h1>Posts</h1>
-      <Post />
-      <Post />
-    </>
+
+  if (!posts.length && !isLoading) return "No posts";
+
+  return isLoading ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {posts?.map((post) => (
+        <Grid key={post._id} item xs={12} sm={12} md={6} lg={3}>
+          <Post post={post} setCurrentId={setCurrentId} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
